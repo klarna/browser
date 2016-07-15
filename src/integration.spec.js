6f845ee6ef273a55c7405ca6f267031cc282b312
Juan Lulkin <maiz@lulk.in>
2016-07-15 14:09:18 +0200

    Removes IE9 on Travis as well

--- src/integration.spec.js
+++ src/integration.spec.js
@@ -5,7 +5,7 @@ browser.isPhantomJS = () =>
 
 const tests = [
   ['isIOS', ['iOS 8.0', 'iOS 8.1', 'iOS 9.2']],
-  ['isIE', ['IE 8', 'IE 9', 'IE 10', 'IE 11']],
+  ['isIE', ['IE 10', 'IE 11']],
   ['isAndroid4Stock', ['Android 4.2', 'Android 4.3', 'Android 4.4']],
   ['isUIWebView', []],
   ['isPhantomJS', ['PhantomJS']]
